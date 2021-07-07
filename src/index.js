8147be0a2769223691d17e40b43132b142e066eb
Robson Gomes <contato@robsongomes.com.br>
2021-07-07 09:14:13 -0300

    adicionando rota para api

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.get('/api/v1',(req,res)=>{
+    res.status(200).json({
+        version:'0.0.1'
+    })
+})
 const port = process.env.PORT || 3131
 app.listen(port,'0.0.0.0',()=>{
     console.log('Carregando na porta', port)
